b84796937db6809fd50f3ad0cf89d9e20cd59adf
meraihan <sayedmahmudraihan@gmail.com>
2020-07-14 19:24:04 +0600

    initial structure

---
+++ src/main/java/com/avihai789/core/githubsearch/GithubSearchApplication.java
@@ -0,0 +1,13 @@
+package com.avihai789.core.githubsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/avihai789/core/githubsearch/GithubSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avihai789.core.githubsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
